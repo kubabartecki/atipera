08e50792f4b3f62acafe83edebbbbc0074b777dd
kubabartecki <169511@stud.prz.edu.pl>
2024-07-30 17:10:26 +0200

    initial project

---
+++ github-scanner/src/test/java/com/bartheme/githubscanner/GithubScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartheme.githubscanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubScannerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
